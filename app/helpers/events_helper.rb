a34e199c8f672e0903f42e9bf4acb13778349f7c
Alisson Bruno <alissonbruno.sa@gmail.com>
2015-08-26 22:13:50 -0300

    Botões de compartilhamento

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+	def share_button event
+		html = <<-HTML
+			<div class="col-md-12">
+        <ul class="social-network social-circle">
+          <li>
+            <a href="http://www.facebook.com/share.php?u=#{event_url(event)}" class="icoFacebook b-share-button bg" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+          </li>
+          <li>
+            <a href="http://twitter.com/intent/tweet?status=Não deixe de participar+#{event_url(event)}" class="icoTwitter bg" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
+        </ul>       
+      </div>
+		HTML
+		html.html_safe
+	end
 end
