09ae768293512e5b24b6435330128c64aa8d97e2
Alisson Bruno <alissonbruno.sa@gmail.com>
2015-08-26 22:27:45 -0300

    Botões de compartilhamento

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 	def facebook_share
-		if @event && action_name == "show"
-			html = <<-HTML 
-				<meta property="og:url"           content="#{ event_url(@event) }" />
-		    <meta property="og:type"          content="website" />
-		    <meta property="og:title"         content="#{ @event.title }" />
-		    <meta property="og:description"   content="#{ @event.description }" />
-		    <meta property="og:image"         content="#{ @event.photo.url(:original, timestamp: false) }" />
-			HTML
-			html.html_safe
-		end
+		# if @event && action_name == "show"
+		# 	html = <<-HTML 
+		# 		<meta property="og:url"           content="#{ event_url(@event) }" />
+		#     <meta property="og:type"          content="website" />
+		#     <meta property="og:title"         content="#{ @event.title }" />
+		#     <meta property="og:description"   content="#{ @event.description }" />
+		#     <meta property="og:image"         content="#{ @event.photo.url(:original, timestamp: false) }" />
+		# 	HTML
+		# 	html.html_safe
+		# end
 	end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,7 +4,7 @@ module EventsHelper
 			<div class="col-md-12">
         <ul class="social-network social-circle">
           <li>
-            <a href="http://www.facebook.com/share.php?u=#{event_url(event)}" class="icoFacebook b-share-button bg" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+            <a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=#{event_url(event)}&p[images][0]=#{event.photo.url(:original)}&p[title]=#{event.title}&p[summary]=#{event.description}" class="icoFacebook b-share-button bg" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
           </li>
           <li>
             <a href="http://twitter.com/intent/tweet?status=Não deixe de participar+#{event_url(event)}" class="icoTwitter bg" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
